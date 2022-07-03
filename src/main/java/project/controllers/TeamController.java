c254b1aa0d3d5b37df88299bb2841b161e5945f7
Sergey <101991940+Sergey060890@users.noreply.github.com>
2022-07-03 22:45:45 +0300

    03/07 work

--- src/main/java/project/controllers/TeamController.java
+++ src/main/java/project/controllers/TeamController.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import project.models.Team;
+import project.service.dto.TeamDTO;
 import project.service.interfaces.TeamService;
+
+import java.util.ArrayList;
 
 @Controller
 public class TeamController {
@@ -20,14 +26,35 @@ public class TeamController {
     }
 
     @RequestMapping("/team")//все записи
-    public String horsesMain(Model model) {
+    public String teamMain(Model model) {
         model.addAttribute("teams", teamService.findAll());
         return "team-main";
     }
 
     @RequestMapping("/team/add")//добавление команды
-    public String blogAdd(Model model) {
+    public String teamAdd(Model model) {
         return "team-add";
+    }
+
+    @PostMapping("/team/add")//добавление (получение из формы)
+    public String teamPostAdd(@RequestParam String name, @RequestParam String city, @RequestParam String country,
+                              String stadium, String coach, Model model) {
+        teamService.createTeam(name, city, country, stadium, coach);
+        return "redirect:/team";
+    }
+
+    @GetMapping("/team/{team_id}")//вывод одной записи
+    public String teamDetails(@PathVariable(value = "team_id") Integer id, Model model) {
+        TeamDTO team = teamService.findTeamById(id);
+        model.addAttribute("team", team);
+        return "team-details";
+    }
+
+    @GetMapping("/team/{team_id}/edit")//edit
+    public String teamEdit(@PathVariable(value = "team_id") Integer id, Model model) {
+        TeamDTO team = teamService.findTeamById(id);
+        model.addAttribute("team", team);
+        return "team-edit";
     }
 
 }
